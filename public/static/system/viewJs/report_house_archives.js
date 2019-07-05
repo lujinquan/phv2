b0e82465adac9fba06968d8e80a198ef419d3def
lsn888 <157290451@qq.com>
2019-07-05 16:43:56 +0800

    页面样式调整

--- public/static/system/viewJs/report_house_archives.js
+++ public/static/system/viewJs/report_house_archives.js
@@ -1,10 +1,12 @@
 /*房屋统计*/
+window.onload = function () {
 var section_name = $('#userName').text();
 var section_name_start = section_name.indexOf('(')+1;
 var section_name_end = section_name.indexOf(')');
 section_name = section_name.substring(section_name_start,section_name_end);
 var time_span = $('#timeYear').val().split('-');
 $('.time').text(time_span[0]+'年'+time_span[1]+'月');
+};
 $("#yueQuery").click(function() {
     $('.Wsdj:gt(0)').remove();
     $('.Syxz:gt(0)').remove();
