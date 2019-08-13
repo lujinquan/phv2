f1c4509d7c81cfe4c2c049710cb75b0caf392124
lsn888 <157290451@qq.com>
2019-08-13 14:26:50 +0800

    样式调整

--- public/static/system/viewjs/report_house_archives.js
+++ public/static/system/viewjs/report_house_archives.js
@@ -6,7 +6,7 @@ var section_name_end = section_name.indexOf(')');
 section_name = section_name.substring(section_name_start,section_name_end);
 var time_span = $('#timeYear').val().split('-');
 $('.time').text(time_span[0]+'年'+time_span[1]+'月');
-};
+}
 $("#yueQuery").click(function() {
     $('.Wsdj:gt(0)').remove();
     $('.Syxz:gt(0)').remove();
