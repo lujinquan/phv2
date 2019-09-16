51cdae178db3d5087a3ea69a99929886a8105354
lu <992924033@qq.com>
2019-09-16 10:37:29 +0800

    租约信息展示

---
+++ application/deal/model/ChangeLease.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\deal\model;
+
+use app\system\model\SystemBase;
+
+class ChangeLease extends SystemBase
+{
+	// 设置模型名称
+    protected $name = 'change_lease';
+
+    // 自动写入时间戳
+    protected $autoWriteTimestamp = true;
+
+    protected $type = [
+        'last_print_time' => 'timestamp:Y-m-d H:i:s',
+        'ctime' => 'timestamp:Y-m-d H:i:s',
+    ];
+
+    public function tenant()
+    {
+        return $this->hasOne('app\house\model\Tenant', 'tenant_id', 'tenant_id')->bind('tenant_number,tenant_tel,tenant_card');
+    }
+
+    public function house()
+    {
+        return $this->hasOne('app\house\model\House', 'house_id', 'house_id')->bind('house_number,house_pre_rent,house_cou_rent');
+    }
+
+    public function checkWhere($data)
+    {
+        if(!$data){
+            $data = request()->param();
+        }
+        $where = [];
+        // 检索楼栋地址
+        if(isset($data['ban_address']) && $data['ban_address']){
+            $where[] = ['ban_address','like','%'.$data['ban_address'].'%'];
+        }
+        //检索管段
+        $insts = config('inst_ids');
+        $instid = (isset($data['ban_inst_id']) && $data['ban_inst_id'])?$data['ban_inst_id']:INST;
+        $where[] = ['d.ban_inst_id','in',$insts[$instid]];
+        $where[] = ['a.change_status','eq',1];
+        return $where;
+    }
+}
--- application/house/admin/Lease.php
+++ application/house/admin/Lease.php
@@ -12,13 +12,51 @@
 
 namespace app\house\admin;
 
+use think\Db;
 use app\system\admin\Admin;
+use app\deal\model\ChangeLease as ChangeLeaseModel;
 
 class Lease extends Admin
 {
 
     public function index()
     {
+    	if ($this->request->isAjax()) {
+            $page = input('param.page/d', 1);
+            $limit = input('param.limit/d', 10);
+            $getData = $this->request->get();
+            $ChangeLeaseModel = new ChangeLeaseModel;
+            $where = $ChangeLeaseModel->checkWhere($getData);
+            //halt($where);
+            $fields = 'a.id,a.change_order_number,a.szno,b.house_use_id,c.tenant_name,c.tenant_tel,c.tenant_card,d.ban_address,d.ban_owner_id,d.ban_inst_id';
+            $data = [];
+            $data['data'] = Db::name('change_lease')->alias('a')->join('house b','a.house_id = b.house_id','left')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','b.ban_id = d.ban_id','left')->field($fields)->where($where)->page($page)->limit($limit)->select();
+            //halt($data['data']);
+            $data['count'] = Db::name('change_lease')->alias('a')->join('house b','a.house_id = b.house_id','left')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','b.ban_id = d.ban_id','left')->where($where)->count('a.id');
+            $data['code'] = 0;
+            $data['msg'] = '';
+            return json($data);
+        }
+
     	return $this->fetch();
     }
+
+    public function detail()
+    {
+    	if ($this->request->isAjax()) {
+    		$id = input('param.id/d');
+    		$data = [];
+        	$data['data'] = ChangeLeaseModel::with(['house','tenant'])->get($id);
+        	$data['code'] = 0;
+            $data['msg'] = '';
+            return json($data);
+    	}
+        $id = input('param.id/d');
+        $row = ChangeLeaseModel::with(['house','tenant'])->get($id);
+        $this->assign('id',$id);
+        $this->assign('data_info',$row);
+    	return $this->fetch();
+    }
+
+
 }
