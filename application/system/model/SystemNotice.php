97ef27bebabe5717723e6e9e46c660b0356ec123
Lucas <6501477+mylucas@user.noreply.gitee.com>
2020-07-02 17:03:54 +0800

    系统公告权限

--- application/system/model/SystemNotice.php
+++ application/system/model/SystemNotice.php
@@ -31,6 +31,9 @@ class SystemNotice extends Model
             case 4:
                 return '全部（包含租户）';
     			break;
+            case 5:
+                return '仅超级管理员';
+                break;
     		default:
     			# code...
     			break;
@@ -69,7 +72,7 @@ class SystemNotice extends Model
         if(INST == 1){
             //
         }elseif(INST == 2){
-            $where[] = ['inst_id','in',[1,2,4]];
+            $where[] = ['inst_id','in',[1,2,4,5]];
         }elseif(INST == 3){
             $where[] = ['inst_id','in',[1,3,4]];
         }elseif(INST == 4){
