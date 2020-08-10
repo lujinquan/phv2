c32c3cb4b6dac9c24f71215dc484e36fee9d0329
lucas <598936602@qq.com>
2020-08-10 08:49:00 +0800

    sd

--- application/rent/model/RentOrderChild.php
+++ application/rent/model/RentOrderChild.php
@@ -43,6 +43,10 @@ class RentOrderChild extends Model
         if(isset($data['pay_way']) && $data['pay_way']){
             $where[] = ['pay_way','eq',$data['pay_way']];
         }
+        // 检索【收欠】支付金额
+        if(isset($data['rent_order_paid']) && $data['rent_order_paid']){
+            $where[] = ['rent_order_paid','eq',$data['rent_order_paid']];
+        }
         // 检索【租户】姓名
         if(isset($data['tenant_name']) && $data['tenant_name']){
             $where[] = ['tenant_name','like','%'.$data['tenant_name'].'%'];
@@ -96,7 +100,7 @@ class RentOrderChild extends Model
 
     public function detail($id)
     {
-        $fields = "a.rent_order_id,a.rent_order_number,a.rent_order_date,a.rent_order_number,a.rent_order_receive,a.rent_order_paid,(a.rent_order_receive-a.rent_order_paid) as rent_order_unpaid,a.is_invoice,a.house_id,from_unixtime(a.ptime,'%Y-%m-%d %H:%i:%S') as ptime,a.pay_way,b.house_use_id,b.house_number,c.tenant_id,c.tenant_name,c.tenant_card,c.tenant_tel,d.ban_address,d.ban_owner_id,d.ban_inst_id";
+        $fields = "a.rent_order_id,a.rent_order_number,a.rent_order_date,a.rent_order_number,a.rent_order_receive,a.rent_order_paid,(a.rent_order_receive-a.rent_order_paid) as rent_order_unpaid,a.is_invoice,a.house_id,from_unixtime(a.ptime,'%Y-%m-%d %H:%i:%S') as ptime,a.pay_way,b.house_use_id,b.house_number,b.house_pre_rent,c.tenant_id,c.tenant_name,c.tenant_card,c.tenant_tel,d.ban_address,d.ban_owner_id,d.ban_inst_id";
         $row = Db::name('rent_order_child')->alias('a')->join('house b','a.house_id = b.house_id','left')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','b.ban_id = d.ban_id','left')->field($fields)->where([['id','eq',$id]])->find();
         //halt($row);
         $row['pay_info'] = Db::name('rent_order')->where([['house_id','eq',$row['house_id']],['tenant_id','eq',$row['tenant_id']]])->field('sum(rent_order_receive-rent_order_paid) as total_unpaid_rent,sum(rent_order_paid) as total_paid_rent')->find();
