b4a5dbfacee1e86e3be0f6898d2770faf8c79732
Lucas <6501477+mylucas@user.noreply.gitee.com>
2021-01-15 10:24:14 +0800

    sd

--- application/function.php
+++ application/function.php
@@ -45,6 +45,39 @@ if (!function_exists('tranTime')) {
 
 }
 
+if (!function_exists('client_os')) {
+    /**
+     * 用户设备类型
+     * @return string
+     */
+    function client_os() {
+
+        $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+        if(strpos($agent, 'windows nt')) {
+            $platform = 'windows';
+        } elseif(strpos($agent, 'macintosh')) {
+            $platform = 'mac';
+        } elseif(strpos($agent, 'ipod')) {
+            $platform = 'ipod';
+        } elseif(strpos($agent, 'ipad')) {
+            $platform = 'ipad';
+        } elseif(strpos($agent, 'iphone')) {
+            $platform = 'iphone';
+        } elseif (strpos($agent, 'android')) {
+            $platform = 'android';
+        } elseif(strpos($agent, 'unix')) {
+            $platform = 'unix';
+        } elseif(strpos($agent, 'linux')) {
+            $platform = 'linux';
+        } else {
+            $platform = 'other';
+        }
+
+        return $platform;
+    }
+}
+
 if (!function_exists('http_request')) {
 
 	function http_request($url,$data = null,$headers=array())
