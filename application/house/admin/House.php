1456a385e87d101c937c74687c9379cdb340d429
lucas <598936602@qq.com>
2021-03-15 09:04:45 +0800

    sd

--- application/house/admin/House.php
+++ application/house/admin/House.php
@@ -98,7 +98,12 @@ class House extends Admin
             //halt($where);
             $data = [];
             
-            $temp_data = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->join('weixin_member_house e','a.house_id = e.house_id','left')->where($where);
+
+            // 子查询
+            $subsql = Db::name('weixin_member_house')->field('house_id,member_id')->group('house_id')->having('count(house_id) > 1')->buildSql();
+
+            $temp_data = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->join([$subsql =>'e'],'a.house_id = e.house_id','left')->where($where);
+            // $temp_data = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->join('weixin_member_house e','a.house_id = e.house_id','left')->where($where);
             if(isset($getData['is_bind_weixin'])){
                 if($getData['is_bind_weixin'] == 1){
                     $temp_data->whereNotNull('e.member_id');
@@ -723,8 +728,12 @@ class House extends Admin
             $fields = 'a.house_id,a.house_number,a.house_cou_rent,a.house_use_id,a.house_unit_id,a.house_floor_id,a.house_lease_area,a.house_area,a.house_diff_rent,a.house_pump_rent,a.house_pre_rent,a.house_oprice,a.house_door,a.house_is_pause,a.house_status,c.tenant_id,c.tenant_name,d.ban_number,d.ban_address,d.ban_damage_id,d.ban_struct_id,d.ban_owner_id,d.ban_inst_id,e.member_id';
 
             // $tableData = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->field($fields)->where($where)->order('house_ctime desc')->select();
+             // 子查询
+            $subsql = Db::name('weixin_member_house')->field('house_id,member_id')->group('house_id')->having('count(house_id) > 1')->buildSql();
 
-            $temp_data = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->join('weixin_member_house e','a.house_id = e.house_id','left')->where($where);
+            $temp_data = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->join([$subsql =>'e'],'a.house_id = e.house_id','left')->where($where);
+            
+            // $temp_data = Db::name('house')->alias('a')->join('tenant c','a.tenant_id = c.tenant_id','left')->join('ban d','a.ban_id = d.ban_id','left')->join('weixin_member_house e','a.house_id = e.house_id','left')->where($where);
             if(isset($getData['is_bind_weixin'])){
                 if($getData['is_bind_weixin'] == 1){
                     $temp_data->whereNotNull('e.member_id');
